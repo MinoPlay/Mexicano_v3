58de52b32652d40712b3001b0c8846b06cab8048
Mino <mkuc@demant.com>
2026-09-16 09:50:28 +0200

    add kasper to administrators list

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 import { networkFirst, shouldHandleRequest } from './js/sw-fetch.js';
 import { addNotification } from './js/services/notification-store.js';
 
-export const APP_VERSION = 96;
+export const APP_VERSION = 97;
 
 const CACHE_NAME = `mexicano-v${APP_VERSION}`;
 const ASSETS = [
